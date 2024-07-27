3d6b69821509509785bd2625b91335661f0ff864
The One <16534421@qq.com>
2024-07-28 03:04:37 +0800

    引入Nuxt

---
+++ _nuxt/Fz-04ALE.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as o,q as c,a as n}from"./rl7IoVOz.js";const a={},_={class:"flex items-center"},r=n("div",{class:"animate-pulse bg-[#0f0] w-2 h-5"},null,-1);function l(e,d){return t(),o("div",_,[c(e.$slots,"default"),r])}const f=s(a,[["render",l]]);export{f as _};
