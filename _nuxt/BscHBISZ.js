e35d9cc100f3e76c65640ac6464819cb24a61eda
The One <16534421@qq.com>
2024-07-29 00:43:51 +0800

    调整页面

---
+++ _nuxt/BscHBISZ.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as o,s as c,a as n}from"./DGIgaWHX.js";const a={},_={class:"flex items-center"},r=n("div",{class:"animate-pulse bg-[#0f0] w-2 h-[20px]"},null,-1);function l(e,d){return t(),o("div",_,[c(e.$slots,"default"),r])}const f=s(a,[["render",l]]);export{f as _};
